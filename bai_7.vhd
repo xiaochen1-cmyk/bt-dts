9b8f6d98081c6b300c13c3af71bd551fe05e3c96
xiaochen1-cmyk <anh182005@gmail.com>
2025-07-03 12:10:18 +0700

    Update bai_7.vhd

--- bai_7.vhd
+++ bai_7.vhd
@@ -1,6 +1,5 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
 entity bai_7 is
   port (
     CLK : in  std_logic;
@@ -24,10 +23,9 @@ begin
 t0<= q2 or q1 or q0;
   t1 <= (not q0 and q1) or  q2;
   t2 <= not q1 and not q0;
-
-  ff0 : T_FF port map (T => t0, CLK => CLK, Q => q0, Q_inv => open);
-  ff1 : T_FF port map (T => t1,  CLK => CLK,  Q => q1, Q_inv => open);
-  ff2 : T_FF port map (T => t2,  CLK => CLK,  Q => q2, Q_inv => open);
+  FF0 : T_FF port map (T => t0, CLK => CLK, Q => q0, Q_inv => open);
+  FF1 : T_FF port map (T => t1,  CLK => CLK,  Q => q1, Q_inv => open);
+  FF2 : T_FF port map (T => t2,  CLK => CLK,  Q => q2, Q_inv => open);
 
   Q <= q2 & q1 & q0;
 end rtl;
