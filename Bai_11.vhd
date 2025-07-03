89c7fbf386462b734b8d3d842d008fd7edcbda12
xiaochen1-cmyk <anh182005@gmail.com>
2025-07-03 12:18:12 +0700

    Update Bai_11.vhd

--- Bai_11.vhd
+++ Bai_11.vhd
@@ -2,7 +2,7 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date: 07/01/2025 08:35:42 PM
+-- Create Date: 07/01/2025 12:29:41 PM
 -- Design Name: 
 -- Module Name: Bai_11 - Behavioral
 -- Project Name: 
@@ -21,15 +21,6 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity Bai_11 is
   Port (  clk : in std_logic;
