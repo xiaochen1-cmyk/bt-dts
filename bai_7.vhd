900be853eaec580992890ab2e148b46562fbca76
xiaochen1-cmyk <anh182005@gmail.com>
2025-07-03 12:11:24 +0700

    Update bai_7.vhd

--- bai_7.vhd
+++ bai_7.vhd
@@ -1,3 +1,22 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 07/02/2025 10:19:39 PM
+-- Design Name: 
+-- Module Name: bai_7 - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 entity bai_7 is
