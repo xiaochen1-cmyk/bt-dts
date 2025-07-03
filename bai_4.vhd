64a431cca8783fb4eed617ee9332f378cb158a88
xiaochen1-cmyk <anh182005@gmail.com>
2025-07-03 12:14:51 +0700

    Update bai_4.vhd

--- bai_4.vhd
+++ bai_4.vhd
@@ -1,3 +1,22 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 07/02/2025 10:09:34 PM
+-- Design Name: 
+-- Module Name: bai_4 - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
@@ -25,9 +44,9 @@ t0 <= not q2 or not q1 or q0;
   t1 <= q0 or ( q1 and q2);
   t2 <= (q1 and q0)or (q2 and q1);
 
-  ff0 : T_FF port map (T => t0, CLK => CLK, Q => q0, Q_inv => open);
-  ff1 : T_FF port map (T => t1,  CLK => CLK,  Q => q1, Q_inv => open);
-  ff2 : T_FF port map (T => t2,  CLK => CLK, Q => q2, Q_inv => open);
+  FF0 : T_FF port map (T => t0, CLK => CLK, Q => q0, Q_inv => open);
+  FF1 : T_FF port map (T => t1,  CLK => CLK,  Q => q1, Q_inv => open);
+  FF2 : T_FF port map (T => t2,  CLK => CLK, Q => q2, Q_inv => open);
 
   Q <= q2 & q1 & q0;
 end rtl;
